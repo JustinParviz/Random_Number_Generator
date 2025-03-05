d2f140881fbea94355b6be80397ad89337209e1d
JustinParviz <jarshravan@gmail.com>
2025-03-05 14:04:29 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// Notes on the random and floor methods of Math:
+
+
+// let randomNum = Math.random();   // To create a random rumber in JavaScript, you can use the random method of Math. 
+                                    // This will generate a random number between 0 and 1.
+                                    // This will most likely give you a number with a long decimal portion.
+
+// console.log(randomNum);
+
+
+// let randomNum = Math.floor(Math.random() * 6) + 1;   // If hypothetically, you wanted to roll a six-sided dice, you would need a random number 
+                                                        // between 1 and 6, not including the decimal portion.
+                                                        // The First Step is that you will multiply Math.random() * 6. What this will do is give 
+                                                        // you a random number between 0 and 6 exclusive.
+                                                        // Then you're not going to want the decimal portion, instead you would like a whole integer.
+                                                        // So the Second Step will be to enclose this equation with the floor method of Math.
+                                                        // So far, this will generate a random number between 0 and 5 but you need 1 through 6.
+                                                        // For the Third Step, you can increase the minimum by adding plus one. Now this should give
+                                                        // you a random number between 1 and 6.
+
+// console.log(randomNum);
+
+
+// If you're looking for a random number between a certain range, for example between 50 and 100, you can do so by applying the following equation:
+
+// const min = 50;
+// const max = 100;
+
+// let randomNum = Math.floor(Math.random() * (max - min)) + min;
+
+// console.log(randomNum);
+
+
+
+// RANDOM NUMBER GENERATOR
+
+
+
+
+
+
+
+
+
