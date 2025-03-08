a6c1a2568eb50998e630e92eaba4dea86e930c48
JustinParviz <jarshravan@gmail.com>
2025-03-08 15:29:20 -0500

    Updated JS function to generate 3 random numbers

--- index.js
+++ index.js
@@ -46,8 +46,12 @@ let randomNum2;
 let randomNum3;
 
 myButton.onclick = function () {
-    randomNum = Math.floor(Math.random() * max) + min;
-    mylabel.textContent = randomNum;
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
 }
 
 
